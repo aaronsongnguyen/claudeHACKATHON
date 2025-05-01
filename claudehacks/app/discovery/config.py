d1135b2ed3fbc13f82319da6e78e0ce43246dc02
aaronsongnguyen <aaronsongnguyen@gmail.com>
2025-04-30 21:10:23 -0700

    fro

--- claudehacks/app/discovery/config.py
+++ claudehacks/app/discovery/config.py
@@ -1,7 +1,6 @@
 import os
 
 # Get API key from environment variable or use default
-ANTHROPIC_API_KEY = os.getenv('ANTHROPIC_API_KEY', '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')
 
 if not ANTHROPIC_API_KEY:
     raise ValueError("ANTHROPIC_API_KEY environment variable is not set")
